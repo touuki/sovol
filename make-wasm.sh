f3ad279eb1574b2a00f53b7f278a44ce1cbb3ba3
touuki <touuki@protonmail.com>
2020-04-13 20:53:15 +0800

    improve automake

---
+++ make-wasm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd `dirname $0`
+output_path=./html/dist
+test -d ${output_path} || mkdir -p ${output_path}
+export EMCC_WASM_BACKEND=1
+em++ -o ${output_path}/libsovol-wasm.js ./wasm/libsovol-wasm.a ./lib/libsovol.a --bind -s TOTAL_MEMORY=200MB -s \
+ALLOW_TABLE_GROWTH=1
--- wasm/make-wasm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export EMCC_WASM_BACKEND=1
-em++ -o libsovol-wasm.js libsovol-wasm.so --bind -s TOTAL_MEMORY=200MB -s \
-ALLOW_TABLE_GROWTH=1
