8ce8e9dfdb8bd691dd0770550743f2837c3a2b01
Tou Uki <touuki@protonmail.com>
2021-09-11 19:16:32 +0800

    fix longitudinal LG field

--- src/Field.cc
+++ src/Field.cc
@@ -132,19 +132,14 @@ class LaguerreGaussianPulseField : public Field {
   double k;  // 2 * PI / lambda
   double h;  // unit is lambda when input
 
-  std::complex<double> amplitude(double x, double y, double z) const {
-    using namespace std;
+  double amplitude(double x, double y, double z) const {
     double r2 = std::pow(x, 2) + std::pow(y, 2);
-    double wz = w0 * std::sqrt(1. + std::pow(z, 2) / (std::pow(zR, 2)));
-    double irz = z / (std::pow(z, 2) + std::pow(zR, 2));
-    double psiz = (std::abs(l) + 2 * p + 1) * std::atan(z / zR);
+    double wz = w0 * std::sqrt(1. + std::pow(z / zR, 2));
     double item1 = l == 0 ? 1. : std::pow(std::sqrt(2. * r2) / wz, std::abs(l));
     double item2 = p == 0 ? 1.
                           : utils::generLaguePoly(std::abs(l), p,
                                                   2. * r2 / std::pow(wz, 2));
-    double extraPhase = l == 0 ? 0. : l * std::atan2(y, x);
-    return a0 * (w0 / wz) * item1 * item2 * std::exp(-r2 / std::pow(wz, 2)) *
-           std::exp(-1.i * (.5 * k * r2 * irz + extraPhase - psiz));
+    return a0 * (w0 / wz) * item1 * item2 * std::exp(-r2 / std::pow(wz, 2));
   };
 
   double envelope(double x, double y, double z, double t) const {
@@ -198,18 +193,52 @@ class LaguerreGaussianPulseField : public Field {
     double x = pos.x;
     double y = pos.y;
     double z = pos.z;
-    complex<double> u = amplitude(x, y, z);
-    complex<double> pupx =
-        (amplitude(x + h, y, z) - amplitude(x - h, y, z)) / (2. * h);
-    complex<double> pupy =
-        (amplitude(x, y + h, z) - amplitude(x, y - h, z)) / (2. * h);
+    double r2 = std::pow(x, 2) + std::pow(y, 2);
+    double irz = z / (std::pow(z, 2) + std::pow(zR, 2));
+    double psiz = (std::abs(l) + 2 * p + 1) * std::atan(z / zR);
+    double lphi = l == 0 || r2 == 0 ? 0. : l * std::atan2(y, x);
+    double u0 = amplitude(x, y, z);
     complex<double> common =
-        std::exp(1.i * ((t - z) * k + iphase)) * envelope(x, y, z, t);
-    complex<double> Ex = -1.i * ex * u * common;
-    complex<double> Ey = ey * u * common;
+        std::exp(1.i * ((t - z - .5 * r2 * irz) * k + iphase - lphi + psiz)) *
+        envelope(x, y, z, t);
+
+    complex<double> pupx, pupy;
+    if (p == 0) {
+      complex<double> factor = k * u0 / (1.i * z - zR);
+      if (l == 0) {
+        pupx = factor * x;
+        pupy = factor * y;
+      } else {
+        if (r2 == 0) {
+          if (std::abs(l) == 1) {
+            double fact = a0 * std::sqrt(2.) / (w0 * (1 + std::pow(z / zR, 2)));
+            pupx = fact;
+            pupy = -1.i * fact;
+          } else {
+            pupx = 0;
+            pupy = 0;
+          }
+        } else {
+          pupx = factor * x + u0 * (x + 1.i * static_cast<double>(l) * y) / r2;
+          pupy = factor * y + u0 * (y - 1.i * static_cast<double>(l) * x) / r2;
+        }
+      }
+    } else {
+      pupx = (amplitude((x + h) * x < 0 ? x * 0.5 : x + h, y, z) -
+              amplitude((x - h) * x < 0 ? x * 0.5 : x - h, y, z)) /
+                 (2. * h) -
+             1.i * u0 * (k * x * irz - l * y / r2);
+      pupy = (amplitude(x, (y + h) * y < 0 ? y * 0.5 : y + h, z) -
+              amplitude(x, (y - h) * y < 0 ? y * 0.5 : y - h, z)) /
+                 (2. * h) -
+             1.i * u0 * (k * y * irz + l * x / r2);
+    }
+
+    complex<double> Ex = -1.i * ex * u0 * common;
+    complex<double> Ey = ey * u0 * common;
     complex<double> Ez = -(ex * pupx + ey * pupy * 1.i) * common;
-    complex<double> Bx = -ey * u * common;
-    complex<double> By = -1.i * ex * u * common;
+    complex<double> Bx = -ey * u0 * common;
+    complex<double> By = -1.i * ex * u0 * common;
     complex<double> Bz = (ey * pupx * 1.i - ex * pupy) * common;
     return EMField<double>{Vector3(Ex.real(), Ey.real(), Ez.real()),
                            Vector3(Bx.real(), By.real(), Bz.real())};
